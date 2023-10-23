42d97a25273be8320e86e5f0df0dfe0b6da6a757
TCTrung <trungtrinh1325@gmail.com>
2023-10-23 11:42:49 +0700

    hi

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+import static src.GenericStackClient.stackOfIStrings;
+import static src.GenericStackClient.stackOfIntegers;
+
+public class Main {
+        public static void main(String[] args) {
+            System.out.println("1. Stack of integers");
+            stackOfIntegers();
+            System.out.println("\n2. Stack of Strings");
+            stackOfIStrings();
+        }
+}
